55abfb665974fb1540152bd62711583a7c6bea1a
jihee <esder1310@naver.com>
2026-03-08 17:49:43 +0900

    fix(HomeView.tsx) : #2 (코드리뷰 반영) CSS 커스텀 프로퍼티 Tailwind 문법 수정

--- src/pages/home/ui/HomeView.tsx
+++ src/pages/home/ui/HomeView.tsx
@@ -1,7 +1,7 @@
 // (로그인O) 홈페이지 뷰
 export default function HomeView() {
   return (
-    <div className="flex flex-col justify-center items-center bg-[--white] text-[--black] min-h-screen ">
+    <div className="flex flex-col justify-center items-center bg-[var(--white)] text-[var(--black)] min-h-screen ">
       홈페이지
     </div>
   );
