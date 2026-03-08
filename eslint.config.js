47353fb5518d7d40a8de9e215bedb5b7af142061
jihee <esder1310@naver.com>
2026-03-08 15:03:29 +0900

    setting(eslint.config.js) : #2 FSD 단방향 의존성을 위한 rules 작업 진행

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -19,5 +19,28 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      "import/no-restricted-paths": [
+        "error",
+        {
+          zones: [
+            /* pages */
+            { target: "./src/pages", from: "./src/app" },
+            /* features */
+            { target: "./src/features", from: "./src/app" },
+            { target: "./src/features", from: "./src/widgets" },
+            /* entities */
+            { target: "./src/entities", from: "./src/app" },
+            { target: "./src/entities", from: "./src/widgets" },
+            { target: "./src/entities", from: "./src/features" },
+            /* shared */
+            { target: "./src/shared", from: "./src/app" },
+            { target: "./src/shared", from: "./src/widgets" },
+            { target: "./src/shared", from: "./src/features" },
+            { target: "./src/shared", from: "./src/entities" },
+          ],
+        },
+      ],
+    },
   },
-])
+]);
