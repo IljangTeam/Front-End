8c8a65009087b3462f08cbedcfc6ccbb64868f04
jiHeeFlee <esder1310@naver.com>
2026-03-28 18:15:38 +0900

    fix : #3 config 충돌 방지 수정

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  compiler: {
-    emotion: true,
-  },
+  // compiler: {
+  //   emotion: true,
+  // }, //설정 충돌로 주석처리
 };
 
 export default nextConfig;
