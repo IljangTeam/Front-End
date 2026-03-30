312f29b2e85ea1aa03265bf05da9e8cd7c22b4a4
jiHeeFlee <esder1310@naver.com>
2026-03-31 00:48:15 +0900

    feat(view) : #7 AUTH 페이지 속 View 구현(Login, Signup)

---
+++ src/app/auth/_ui/LoginView.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useState } from "react";
+
+// ===== hook =====
+import { useLoginFormData } from "../_model/useLoginFormData";
+
+// ===== ui components =====
+import {
+  AuthInputComponenet as LoginInput,
+  AuthSubmitButton as SubmitButton,
+} from "./LoginComponents";
+
+type LoginFormData = {
+  email: string;
+  password: string;
+};
+
+export default function LoginView() {
+  const {
+    updateEmail,
+    updatePassword,
+    handleSubmit,
+    isValid,
+    showPassword,
+    togglePasswordVisibility,
+  } = useLoginFormData();
+
+  return (
+    <div className="flex flex-col justify-center items-center min-w-[374.667px]">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-between items-center min-w-full gap-[14px]"
+      >
+        <LoginInput
+          title="이메일"
+          placeholder="example@email.com"
+          onChange={(e) => updateEmail(e.target.value)}
+        />
+        <LoginInput
+          title="비밀번호"
+          placeholder="비밀번호 입력"
+          type={showPassword ? "text" : "password"}
+          onChange={(e) => updatePassword(e.target.value)}
+          suffix={
+            <button type="button" onClick={togglePasswordVisibility}>
+              {showPassword ? (
+                "숨기기"
+              ) : (
+                <img src="/assets/password-hide.svg" alt="비밀번호 보기" />
+              )}
+            </button>
+          }
+        />
+        <span className="text-[var(--color-text-tertiary)] text-right font-['Pretendard_Variable'] text-[12px] right font-medium leading-[18px] underline w-full cursor-pointer">
+          비밀번호를 잊으셨나요?
+        </span>
+
+        <SubmitButton contents="이메일로 로그인" />
+      </form>
+    </div>
+  );
+}
