5a11875d1d82dad2b6b7dfca4d7f1ca162a7ddb2
jiHeeFlee <esder1310@naver.com>
2026-04-02 13:28:02 +0900

    move(AuthSettingPage) : #13 FSD 구조에 맞게 변경

---
+++ src/Views/authSetting/ui/AuthSettingView.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function AuthSettingView() {
+  return <span>auth setting view</span>;
+}
---
+++ src/app/auth/setting/page.tsx
@@ -0,0 +1,5 @@
+import { AuthSettingView } from "@/Views/authSetting";
+
+export default function AuthSettingPage() {
+  return <AuthSettingView />;
+}
