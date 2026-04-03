92f72334e289afb5d4179c92c8ce15dd73345c00
jiHeeFlee <esder1310@naver.com>
2026-04-03 17:24:12 +0900

    setting(next.config.ts) : #18 번들러 svg 설정

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // compiler: {
-  //   emotion: true,
-  // }, //설정 충돌로 주석처리
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
