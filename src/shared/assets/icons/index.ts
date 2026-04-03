a03cd88e184618e8c5cd970c90d94018a85fb02c
jiHeeFlee <esder1310@naver.com>
2026-04-03 17:28:29 +0900

    feat(Icon) : #18 아이콘 배럴 파일 추가

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -2,3 +2,5 @@
 export { default as MapPinIcon } from "./MapPin.svg";
 export { default as ClockIcon } from "./Clock.svg";
 export { default as TargetIcon } from "./Target.svg";
+export { default as ArrowRightIcon } from "./ArrowRight.svg";
+export { default as CheckIcon } from "./Check.svg";
